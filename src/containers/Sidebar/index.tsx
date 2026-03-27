2cd0b0bbf036404acaa45d8322ea7f6df1a6517b
Brenda Cristtyny <brenda.cristtyny00@gmail.com>
2026-03-26 21:49:29 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/B73nd4.png" />
+    <Titulo>Brenda Cristtyny</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi culpa
+      reprehenderit ipsa libero aliquam quo impedit quaerat sed, eveniet nostrum
+      odit minus corrupti fugiat dolores est alias consequuntur nam itaque!
+    </p>
+  </section>
+)
+
+export default Sobre
