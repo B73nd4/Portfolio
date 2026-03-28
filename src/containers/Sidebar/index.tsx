547445515be676b1cd866d61e010243367863ab4
Brenda Cristtyny <brenda.cristtyny00@gmail.com>
2026-03-28 19:13:12 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/B73nd4.png" />
-    <Titulo fontSize={20}>Brenda Cristtyny</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Brenda Cristtyny</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Brenda Cristtyny
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -6,10 +6,17 @@ export const EstiloGlobal = createGlobalStyle`
     margin: 0;
     padding:0;
     font-family: "Inter", sans-serif;
+    list-style: none;
     }
 
-    body{
-    padding:80px}
+    body {
+    padding-top: 80px;
+    padding-bottom: 80px;
+
+    @media (max-width: 768px;) {
+      padding-top: 16px;
+      }
+    }
 `
 
 export default EstiloGlobal
@@ -22,7 +29,8 @@ export const Container = styled.div`
   grid-template-columns: 128px auto;
   column-gap: 56px;
 
-  img {
-    max-width: 100%;
+  @media (max-width: 768px) {
+    max-width: 80%;
+    display: block;
   }
 `
